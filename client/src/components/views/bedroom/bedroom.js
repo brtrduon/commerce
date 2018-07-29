2870cddb42662f829a7363a0e8878445f19f3898
brtrduon <brianduongt@gmail.com>
2018-07-29 16:55:24 -0700

    more stuff on details. text is overflowing

--- client/src/components/views/bedroom/bedroom.js
+++ client/src/components/views/bedroom/bedroom.js
@@ -8,28 +8,38 @@ class Bedroom extends Component {
 
   jQuery() {
     $(document).ready(() => {
-      // $('div').click(() => {
-      //   var id = $(this).attr('id');
-      //   $('.sidebar__link').removeClass('active');
-      //   $("[href=#"+id+"").addClass('active');
-      // })
+
     })
   }
 
   render() {
     return (
-      <div className="section-bedroom">
-          <ul className='sidebar'>
-            <li className='sidebar__item'>
-              <a href='#details' className='sidebar__link'>DETAILS</a>
-            </li>
-            <li className='sidebar__item'>
-              <a href='#description' className='sidebar__link'>DESCRIPTION</a>
-            </li>
-            <li className='sidebar__item'>
-              <a href='#customtab' className='sidebar__link'>CUSTOM TAB</a>
-            </li>
-          </ul>
+      <div className='section-bedroom'>
+        <ul className='sidebar'>
+              <li className='sidebar__item'>
+                <a href='#details' className='sidebar__link'>DETAILS</a>
+              </li>
+              <li className='sidebar__item'>
+                <a href='#description' className='sidebar__link'>DESCRIPTION</a>
+              </li>
+              <li className='sidebar__item'>
+                <a href='#customtab' className='sidebar__link'>CUSTOM TAB</a>
+              </li>
+            </ul>
+        <div className="bedroom">
+            <div id='details' className='bedroom__details'>
+              <img src='./../../../img/bedroom.jpg' alt='bedroom.jpg' />
+              <div className='bedroom__details__text'>
+                <h1>Nudie Extendable Sofa for 3 persons.</h1>
+              </div>
+            </div>
+            <div id='description' className='bedroom__description'>
+              <h1>description</h1>
+            </div>
+            <div id='customtab' className='bedroom__customtab'>
+              <h1>Custom tab</h1>
+            </div>
+        </div>
       </div>
     );
   }
