c5d5a259580703faee13bce05202fa764be52c62
brtrduon <brianduongt@gmail.com>
2018-07-31 15:33:33 -0700

    active class

--- client/src/components/views/bedroom/bedroom.js
+++ client/src/components/views/bedroom/bedroom.js
@@ -9,8 +9,9 @@ class Bedroom extends Component {
 
   jQuery() {
     $(document).ready(() => {
+      // viewport check
       $.fn.isInViewport = function() {
-        var elementTop = $(this).offset().top;
+        var elementTop = $(this).offset().top + 600;
         var elementBottom = elementTop + $(this).outerHeight();
     
         var viewportTop = $(window).scrollTop();
@@ -18,8 +19,6 @@ class Bedroom extends Component {
     
         return elementBottom > viewportTop && elementTop < viewportBottom;
       };
-
-
 
       $('a[href^="#"]').on('click', function(e) {
         e.preventDefault();
@@ -34,14 +33,23 @@ class Bedroom extends Component {
     });
 
     $(window).scroll(() => {
-      if ($('#related').isInViewport()) {
-        $('.sidebar__link--related').addClass('active');
-      }
-      else {
+      if ($('#details').isInViewport()) {
+        $('.sidebar__link--details').addClass('active');
+        $('.sidebar__link--description').removeClass('active');
         $('.sidebar__link--related').removeClass('active');
       }
-    })
-
+      if ($('#description').isInViewport()) {
+        $('.sidebar__link--details').removeClass('active');
+        $('.sidebar__link--description').addClass('active');
+        $('.sidebar__link--related').removeClass('active');
+      }
+      if ($('#related').isInViewport()) {
+        $('.sidebar__link--details').removeClass('active');
+        $('.sidebar__link--description').removeClass('active');
+        $('.sidebar__link--related').addClass('active');
+      }
+      
+    });
   }
 
   render() {
@@ -59,36 +67,36 @@ class Bedroom extends Component {
           </li>
         </ul>
         <div className="bedroom">
-            <div id='details' className='bedroom__details'>
-              <img src='./../../../img/bedroom.jpg' alt='bedroom.jpg' />
-              <div className='bedroom__details__text'>
-                <h1 className='bedroom__details__title'>Nudie Extendable Sofa for 3 persons.</h1>
-                <h3 className='bedroom__details__price'><s>$169</s> $149</h3>
-                <div className='bedroom__details__assembly'>
-                  <ul>
-                    <li>
-                      <b>ASSEMBLY</b>: Carpenter Assembly
-                    </li>
-                    <li>
-                      <b>WARRANTY</b>: 12 Months' Warrant
-                    </li>
-                  </ul>
-                </div>
+          <div id='details' className='bedroom__details'>
+            <img src='./../../../img/bedroom.jpg' alt='bedroom.jpg' />
+            <div className='bedroom__details__text'>
+              <h1 className='bedroom__details__title'>Nudie Extendable Sofa for 3 persons.</h1>
+              <h3 className='bedroom__details__price'><s>$169</s> $149</h3>
+              <div className='bedroom__details__assembly'>
+                <ul>
+                  <li>
+                    <b>ASSEMBLY</b>: Carpenter Assembly
+                  </li>
+                  <li>
+                    <b>WARRANTY</b>: 12 Months' Warrant
+                  </li>
+                </ul>
               </div>
             </div>
-            <div id='description' className='bedroom__description'>
-              <img src='./../../../img/bedroom1.jpg' alt='bedroom1.jpg' />
-              <h1 className='bedroom__description__title'><span className='bedroom__description__title-span'>At vero eos et accusamus et iusto odio dignissimos.</span></h1>
-              <p className='bedroom__description__paragraph bedroom__description__paragraph--1'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-              <p className='bedroom__description__paragraph'>
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit
-              </p>
-            </div>
-            <div id='related' className='bedroom__related'>
-              <h1>Related</h1>
-            </div>
+          </div>
+          <div id='description' className='bedroom__description'>
+            <img src='./../../../img/bedroom1.jpg' alt='bedroom1.jpg' />
+            <h1 className='bedroom__description__title'><span className='bedroom__description__title-span'>At vero eos et accusamus et iusto odio dignissimos.</span></h1>
+            <p className='bedroom__description__paragraph bedroom__description__paragraph--1'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p className='bedroom__description__paragraph'>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit
+            </p>
+          </div>
+          <div id='related' className='bedroom__related'>
+            <h1>Related</h1>
+          </div>
           <div className='footer'>
             <h1 className='footer__title'>FURNISHING YOUR DREAMS</h1>
             <div className='row'>
